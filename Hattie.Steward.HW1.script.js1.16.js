1296c52d6db2ae4806f1997cdba7405a558327a3
hmstew <hmsteward@outlook.com>
2025-01-16 15:08:02 -0800

    Add files via upload
    HW 1 -script.js file completed

---
+++ Hattie.Steward.HW1.script.js1.16.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>HW 1 = Tell me about yourself</h1>
+
+<p>My name is</p>
+
+<p id="demo1"></p>
+
+<script>
+let firstname = `Hattie`;
+document.getElementById("demo1").innerHTML = firstname;
+</script>
+
+<p id="demo2"></p>
+
+<script>
+let lastname = `Steward`;
+document.getElementById("demo2").innerHTML = lastname;
+</script>
+
+<p> I have been at SCC for 1</p>
+<p id="demo3"></p>
+
+<p id="demo4"></p>
+<p> Is to obtain a Web Design AAS-T degree here at SCC</p>
+
+<script>
+document.getElementById("demo3").innerHTML = "year of study";
+
+document.getElementById("demo4").innerHTML = "My goal";
+</script>
+
+</body>
+</html>
