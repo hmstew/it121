3bf930aef65478571f80e9a424bf4a86bdec798e
hmstew <hmsteward@outlook.com>
2025-02-09 10:05:01 -0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
 
-alert ("My name is Hattie Steward. I am half way through Winter quarter 2025. My goal is to complete IT121")
+alert ("My name is Hattie Steward. I enrolled at SCC Winter quarter 2025. My goal is to complete my IT121 class.")
 
